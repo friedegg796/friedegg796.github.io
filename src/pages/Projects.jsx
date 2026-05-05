f1f538e3d903ef812541b18033b838a6a2f386de
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-05-05 01:09:38 +0000

    Mon, May 4, 2026, 9:09 PM -04:00

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+import './Pages.css'
+
+function Projects() {
+    return (
+        <div className="Projects">
+            <h1>Projects</h1>
+            <p>Here are some of my projects:</p>
+            <ul>
+                <li> WIP </li>
+            
