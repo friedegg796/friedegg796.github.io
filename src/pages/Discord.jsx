9474890a7573b8e7f80a292700fd301938a6ddd9
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-07-04 16:24:21 +0000

    Sat, Jul 4, 2026, 12:24 PM -04:00

---
+++ src/pages/Discord.jsx
@@ -0,0 +1,23 @@
+import './Pages.css'
+
+function Index() {
+  return (
+    <>
+
+   <section id="top">
+        <div>
+          <h1>Hasnain Alahi</h1>
+          <p>
+          
+
+       <br></br>
+       A lot of this is a WIP
+
+          </p>
+
+      </>
+      )
+       }
+
+
+export default Index
