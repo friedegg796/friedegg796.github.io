48705ca667b473fc75dc8e27e6a0cf63c15498b2
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-07-09 12:17:14 +0000

    Thu, Jul 9, 2026, 8:17 AM -04:00

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1> Friedegg796 </h1>
+
+       <p> HI! And to my Muslims, Asalamualaykum Warahmatulahi Wabarakatuh!</p>
+
+       <p> This place is ery much a WIP, any tips are appreciated! </p>
+
+       <p> </
+    </>
+  )
+}
+
+export default App
