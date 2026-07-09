d6f77bcedd28648fa1837cf24da20e10a5fda273
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-07-09 13:10:13 +0000

    Thu, Jul 9, 2026, 9:10 AM -04:00

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
        <p> HI! And to my Muslims, Asalamualaykum Warahmatulahi Wabarakatuh(May peace, mercy,and blessings be upon you)!</p>
 
-      <p> I'm an aspiring developer determined to spread benefical knowledge</p>
+      <p> I'm an aspiring developer determined to spread benefical knowledge </p>
 
        <p> This place is very much a WIP, any tips are appreciated! </p>
 
