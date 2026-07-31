6b5e50abbd9cd5ab80b6600b449300bd313a4ab6
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-07-31 11:04:10 +0000

    Fri, Jul 31, 2026, 5:04 PM +06:00

--- src/pages/Personal.jsx
+++ src/pages/Personal.jsx
@@ -14,14 +14,14 @@ return (
 
  <p> Hi! To my Muslims, Asalamualaykum Warahmatulahi Wabarakatuh (May peace, mercy,and blessings be upon you)!</p>
 
- <p> I'm a guy in High school who codes to please Allah;
+ <p> I'm a Bangladeshi (That lives in America) guy in High school who codes to please Allah;
 
   also trying to get into cybersecurity
 
   (YOU GET PAID FOR BREAKING INTO THINGS????? SIGN ME UP!)</p>
 
 
-  <h2> </h2>
+  <h2> Likes and Dislikes </h2>
 <p>
 
 
