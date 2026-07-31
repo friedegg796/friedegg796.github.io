3858e3e6662cb40be76c4e175efe6716b1718fec
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-07-31 11:00:41 +0000

    Fri, Jul 31, 2026, 5:00 PM +06:00

--- src/pages/Personal.jsx
+++ src/pages/Personal.jsx
@@ -7,9 +7,25 @@ return (
      <h1> Personal Page </h1>
 
 
-   <p> How did you find this? well</p>
+   <p> A page literally about me personally </p>
 
      </header>
+
+
+ <p> Hi! To my Muslims, Asalamualaykum Warahmatulahi Wabarakatuh (May peace, mercy,and blessings be upon you)!</p>
+
+ <p> I'm a guy in High school who codes to please Allah;
+
+  also trying to get into cybersecurity
+
+  (YOU GET PAID FOR BREAKING INTO THINGS????? SIGN ME UP!)</p>
+
+
+  <h2> </h2>
+<p>
+
+
+  </p>
     </>
 
 
